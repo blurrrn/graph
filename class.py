739b8a671077330e4831ce7695fa3cbdabaadf43
Vlada <103361796+blurrrn@users.noreply.github.com>
2024-09-30 10:01:44 +0400

    Update class.py

--- class.py
+++ class.py
@@ -1,13 +1,13 @@
 class Graph:
+    # инициализация графа
     def __init__(self, directed=False, weighted=False):
-        # Инициализация графа
         self.adj_list = {}  # Список смежности
-        self.directed = directed  # Флаг для ориентированного графа
-        self.weighted = weighted  # Флаг для взвешенного графа
+        self.directed = directed  # флаг для ориентированного графа
+        self.weighted = weighted  # флаг для взвешенного графа
 
+        # создание графа из файла
     @classmethod
     def from_file(cls, filename):
-        # Метод класса для создания графа из файла
         graph = cls()
         with open(filename, 'r') as file:
             for line in file:
@@ -25,21 +25,22 @@ class Graph:
                         graph.add_edge(u, v, int(w))
         return graph
 
+        # метод для создания копии графа
     def copy(self):
-        # Метод для создания копии графа
         new_graph = Graph(self.directed, self.weighted)
+        # копирование списка смежности
         new_graph.adj_list = {u: v.copy() for u, v in self.adj_list.items()}
         return new_graph
 
+        # добавление вершины
     def add_versh(self, versh):
-        # Метод для добавления вершины
         if versh not in self.adj_list:
             self.adj_list[versh] = {} if self.weighted else set()
         else:
             print(f"Вершина {versh} уже существует.")
 
+    # добавление ребра
     def add_edge(self, u, v, weight=None):
-        # Метод для добавления ребра
         if u not in self.adj_list:
             self.add_versh(u)
         if v not in self.adj_list:
@@ -55,7 +56,7 @@ class Graph:
                 self.adj_list[v].add(u)
 
     def remove_versh(self, versh):
-        # Метод для удаления вершины
+        # удаление вершины
         if versh in self.adj_list:
             del self.adj_list[versh]
             for v in self.adj_list:
@@ -67,8 +68,8 @@ class Graph:
         else:
             print(f"Вершина {versh} не существует.")
 
+        # удаление ребра
     def remove_edge(self, u, v):
-        # Метод для удаления ребра
         if u in self.adj_list and v in self.adj_list[u]:
             if self.weighted:
                 del self.adj_list[u][v]
@@ -81,41 +82,53 @@ class Graph:
         else:
             print(f"Ребро ({u}, {v}) не существует.")
 
+        # сохранение графа в файл
+
     def to_file(self, filename):
-        # Метод для сохранения графа в файл
         with open(filename, 'w') as file:
             if self.directed:
                 file.write("directed\n")
             if self.weighted:
                 file.write("weighted\n")
-            for u, edges in self.adj_list.items():
-                for v, weight in (edges.items() if self.weighted else [(v, None) for v in edges]):
-                    if weight is not None:
-                        file.write(f"{u} {v} {weight}\n")
-                    else:
-                        file.write(f"{u} {v}\n")
 
+            est = set()
+
+            for u, edges in self.adj_list.items():
+
+                if not edges:
+                    file.write(f"{u}\n")
+                else:
+                    for v, weight in (edges.items() if self.weighted else [(v, None) for v in edges]):
+
+                        if (u, v) not in est and (v, u) not in est:
+                            if weight is not None:
+                                file.write(f"{u} {v} {weight}\n")
+                            else:
+                                file.write(f"{u} {v}\n")
+
+                            est.add((u, v))
+
+        # отображение графа пользовател.
     def display(self):
-        # Метод для отображения графа
         for versh, edges in self.adj_list.items():
             if self.weighted:
                 print(f"{versh}: {', '.join(f'{v}({w})' for v, w in edges.items())}")
             else:
                 print(f"{versh}: {', '.join(edges)}")
 
-# Минималистичный консольный интерфейс пользователя
+# консольный интерфейс
 def main():
     graph = Graph()
 
     while True:
-        print("\n1. Добавить вершину")
-        print("2. Добавить ребро")
-        print("3. Удалить вершину")
-        print("4. Удалить ребро")
-        print("5. Показать графэ")
-        print("6. Сохранить в файл")
-        print("7. Загрузить из файла")
-        print("8. Выйти")
+        print("\n1 - Добавить вершину")
+        print("2 - Добавить ребро")
+        print("3 - Удалить вершину")
+        print("4 - Удалить ребро")
+        print("5 - Показать граф")
+        print("6 - Сохранить в файл")
+        print("7 - Загрузить из файла")
+        print("8 - Выйти")
 
         choice = input("Введите ваш выбор: ")
 
@@ -125,7 +138,7 @@ def main():
         elif choice == '2':
             u = input("Введите начальную вершину: ")
             v = input("Введите конечную вершину: ")
-            weight = input("Введите вес (оставьте пустым, если граф невзвешенный): ")
+            weight = input("Введите вес (ничего, если граф невзвешенный): ")
             graph.add_edge(u, v, int(weight) if weight else None)
         elif choice == '3':
             versh = input("Введите вершину для удаления: ")
